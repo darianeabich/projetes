068e6f08a6b2721cd0d1223575ecc7e629a47022
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-10-15 18:09:20 -0300

    feat(form-login): first steps

--- libs/modules/feature/welcome/src/lib/form-login/form-login.component.ts
+++ libs/modules/feature/welcome/src/lib/form-login/form-login.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'projetes-form-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-login.component.html',
   styleUrl: './form-login.component.scss',
 })
-export class FormLoginComponent {}
+export class FormLoginComponent {
+  private fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    password: [
+      '',
+      Validators.compose([Validators.required, Validators.minLength(6)]),
+    ],
+  });
+
+  login() {
+    console.log(this.loginForm.value);
+  }
+}
