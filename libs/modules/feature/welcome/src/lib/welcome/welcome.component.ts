f42f4b356e5a7b3c5f430e629d0dfab0c5b43beb
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-10-15 18:08:50 -0300

    refactor(welcome): increasing

--- libs/modules/feature/welcome/src/lib/welcome/welcome.component.ts
+++ libs/modules/feature/welcome/src/lib/welcome/welcome.component.ts
@@ -1,13 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeService } from '@projetes/shared-service';
 import { PresentationCarrouselComponent } from '@projetes/ui-welcome';
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'projetes-welcome',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PresentationCarrouselComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PresentationCarrouselComponent,
+    ButtonModule,
+    CarouselModule,
+  ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {}
+export class WelcomeComponent implements OnInit {
+  private welcomeService: WelcomeService = inject(WelcomeService);
+  welcomePhrases: any;
+
+  ngOnInit(): void {
+    this.welcomeService.getPhrases().subscribe((data) => {
+      this.welcomePhrases = data;
+    });
+  }
+}
