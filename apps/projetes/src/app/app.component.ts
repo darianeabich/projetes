4b98a4490529218d832dc2a90be90eb1f1943fc1
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-10-15 17:00:29 -0300

    feat(welcome): images carousel

--- apps/projetes/src/app/app.component.ts
+++ apps/projetes/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { LayoutModule } from '@projetes/layout';
+import { PrimeNGConfig } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, LayoutModule, NgIf, ButtonModule],
+  imports: [CommonModule, RouterModule, LayoutModule, NgIf, ButtonModule],
   selector: 'projetes-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   router = inject(Router);
+  private primeconfig = inject(PrimeNGConfig);
   showMenu = true;
+
+  ngOnInit(): void {
+    this.primeconfig.ripple = true;
+  }
 
   showLogin() {
     this.router.navigate(['boasvindas/login']);
--- libs/modules/ui/welcome/src/lib/presentation-carrousel/presentation-carrousel.component.ts
+++ libs/modules/ui/welcome/src/lib/presentation-carrousel/presentation-carrousel.component.ts
@@ -1,59 +1,83 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { CardModule } from 'primeng/card';
-import { CarouselModule } from 'primeng/carousel';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'projetes-presentation-carrousel',
   standalone: true,
-  imports: [CommonModule, CardModule, CarouselModule],
+  imports: [CommonModule, NgFor],
   templateUrl: './presentation-carrousel.component.html',
   styleUrl: './presentation-carrousel.component.scss',
 })
-export class PresentationCarrouselComponent implements OnInit {
-  images: any[] = [];
-  responsiveOptions: any[] = [];
+export class PresentationCarrouselComponent implements OnInit, OnDestroy {
+  images = [
+    {
+      path: '../assets/images/01.png',
+      alt: 'Description for Image 1',
+      title:
+        'Guia para a criação de projetos tecnológicos orientados pelo Design Thinking, um modelo de pensamento centrado no usuário.',
+    },
+    {
+      path: '../assets/images/02.png',
+      alt: 'Description for Image 2',
+      title:
+        'Utilização de diversas técnicas de processo de desenvolvimento projeto de software.',
+    },
+    {
+      path: '../assets/images/03.png',
+      alt: 'Description for Image 3',
+      title:
+        'Estimulação do engajamento na aprendizagem através da gamificação.',
+    },
+    {
+      path: '../assets/images/04.png',
+      alt: 'Description for Image 4',
+      title: 'Empatia. Criatividade. Inovação.',
+    },
+  ];
+
+  currentIndex = 0;
+  intervalId: any;
 
   ngOnInit(): void {
-    this.responsiveOptions = [
-      {
-        breakpoint: '1024px',
-        numVisible: 1,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '768px',
-        numVisible: 1,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '560px',
-        numVisible: 1,
-        numScroll: 1,
-      },
-    ];
+    // this.responsiveOptions = [
+    //   {
+    //     breakpoint: '1024px',
+    //     numVisible: 1,
+    //     numScroll: 1,
+    //   },
+    //   {
+    //     breakpoint: '768px',
+    //     numVisible: 1,
+    //     numScroll: 1,
+    //   },
+    //   {
+    //     breakpoint: '560px',
+    //     numVisible: 1,
+    //     numScroll: 1,
+    //   },
+    // ];
 
-    this.images = [
-      {
-        source: 'assets/entrada/01.png',
-        alt: 'Description for Image 1',
-        title: 'Title 1',
-      },
-      {
-        source: 'assets/entrada/02.png',
-        alt: 'Description for Image 2',
-        title: 'Title 2',
-      },
-      {
-        source: 'assets/entrada/03.png',
-        alt: 'Description for Image 3',
-        title: 'Title 3',
-      },
-      {
-        source: 'assets/entrada/04.png',
-        alt: 'Description for Image 4',
-        title: 'Title 4',
-      },
-    ];
+    console.log('images', this.images);
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 10000); // Troca a cada 10 segundos
+  }
+
+  setCurrentIndex(index: number) {
+    this.currentIndex = index;
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.startAutoSlide(); // Reinicia o intervalo ao trocar manualmente
+    }
   }
 }
