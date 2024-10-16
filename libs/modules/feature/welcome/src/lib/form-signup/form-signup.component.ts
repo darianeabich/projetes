1c26da424b181b021bcfc68f64857e5a7da7b680
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-10-16 10:58:50 -0300

    feat(signup): create form

--- libs/modules/feature/welcome/src/lib/form-signup/form-signup.component.ts
+++ libs/modules/feature/welcome/src/lib/form-signup/form-signup.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'projetes-form-signup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TabViewModule, ReactiveFormsModule, ButtonModule],
   templateUrl: './form-signup.component.html',
   styleUrl: './form-signup.component.scss',
 })
-export class FormSignupComponent {}
+export class FormSignupComponent {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+
+  participantForm = this.fb.group({
+    nome: ['', Validators.required],
+    cod_institucional: [
+      '',
+      Validators.compose([Validators.required, Validators.minLength(10)]),
+    ],
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    senha: [
+      '',
+      Validators.compose([Validators.required, Validators.minLength(8)]),
+    ],
+    // passwordConfirm: ['', Validators.required, Validators.minLength(8)],
+    status: [true],
+    perfil: ['PARTICIPANTE'],
+  });
+
+  facilitatorForm = this.fb.group({
+    nome: ['', Validators.required],
+    cod_institucional: [
+      '',
+      Validators.compose([Validators.required, Validators.minLength(10)]),
+    ],
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+  });
+
+  signup() {
+    console.log(this.participantForm.value);
+  }
+
+  apply() {
+    console.log(this.facilitatorForm.value);
+  }
+
+  redirect() {
+    this.router.navigate(['welcome/login']);
+  }
+}
